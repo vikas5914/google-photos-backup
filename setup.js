73c254129b5973858b351160230a8755a50b3a24
akhil-rana <akhilrana0001@gmail.com>
2024-12-17 19:02:16 +0530

    FIX: google account login

--- setup.js
+++ setup.js
@@ -1,6 +1,12 @@
 import path from 'path'
 import { chromium } from 'playwright-extra'
 import stealth from 'puppeteer-extra-plugin-stealth'
+import ua from 'user-agents'
+
+const userAgent = new ua({
+  platform: 'MacIntel', // 'Win32', 'Linux ...'
+  deviceCategory: 'desktop', // 'mobile', 'tablet'
+});
 
 chromium.use(stealth())
 
@@ -10,8 +16,20 @@ const userDataDir = './session';
   const browser = await chromium.launchPersistentContext(path.resolve(userDataDir), {
     headless: false,
     channel: 'chrome', // possible values: chrome, msedge and chromium
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  })
+    args: [
+      '--disable-features=IsolateOrigins,site-per-process',
+      '--disable-blink-features=AutomationControlled',
+      '--no-sandbox',         // May help in some environments
+      '--disable-infobars',    // Prevent infobars
+      '--disable-extensions',   // Disable extensions
+      '--start-maximized',      // Start maximized
+      '--window-size=1280,720'  // Set a specific window size
+    ],
+    userAgent: userAgent.toString(),
+    viewport: { width: 1280, height: 720 },
+    deviceScaleFactor: 1,
+  });
+
   const page = await browser.newPage()
   await page.goto('https://photos.google.com/')
 
