8ca3bdd2ba60ce71daa215bedd34a0fde881ad30
akhil-rana <akhilrana0001@gmail.com>
2024-12-18 05:20:37 +0530

    Add proper ui and more

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import fs from 'node:fs'
 import { exiftool } from 'exiftool-vendored'
 import ua from 'user-agents'
 import { exec } from 'node:child_process'
+import term from 'terminal-kit'
 
 const userAgent = new ua({
   platform: 'MacIntel', // 'Win32', 'Linux ...'
@@ -44,7 +45,7 @@ const saveProgress = async (page) => {
   if (currentUrl.startsWith('https://photos.google.com')) {
     await fsP.writeFile('.lastdone', currentUrl, 'utf-8');
   } else {
-    console.log('Current URL does not start with https://photos.google.com, not saving progress.');
+    addLog('Current URL does not start with https://photos.google.com, not saving progress.', 'info');
   }
 }
 
@@ -61,14 +62,21 @@ const getMonthAndYear = async (filePath, metadata, page) => {
     month = metadata.CreateDate.month
     dateType = "CreateDate"
   } else {
-    // if metadata is not available, we try to get the date from the html
-    const dateString = await page.$eval('div[aria-label^="Date taken:"]', el => el.textContent.trim())
-    console.log(`Metadata not found for in exif of ${filePath.split('/').pop()}, so getting from html: ${dateString}`)
-    const date = new Date(dateString)
-    if (date.toString() !== 'Invalid Date') {
-      year = date.getFullYear()
-      month = date.getMonth() + 1
-      dateType = "HTML"
+    try {
+      // if metadata is not available, we try to get the date from the html
+      const dateString = await page.$eval('div[aria-label^="Date taken:"]', el => el.textContent.trim())
+      addLog(`Metadata not found for in exif of ${filePath.split('/').pop()}, so getting from html: ${dateString}`, 'info')
+      const date = new Date(dateString)
+      if (date.toString() !== 'Invalid Date') {
+        year = date.getFullYear()
+        month = date.getMonth() + 1
+        dateType = "HTML"
+      }
+    } catch (error) {
+      addLog(`Failed to get date from HTML for ${filePath.split('/').pop()}: ${error.message}`, 'error')
+      year = 1970
+      month = 1
+      dateType = "default"
     }
   }
   return { year, month, dateType }
@@ -76,39 +84,168 @@ const getMonthAndYear = async (filePath, metadata, page) => {
 
 let FILE_PATH = ''
 const downloadQueue = [];
+let activeDownloads = 0;
+
+// Terminal UI Setup
+const terminal = term.terminal
+let activeDownloadList = []
+let completedDownloadList = []
+let logs = []
+
+let totalDownloaded = 0
+
+const renderUI = () => {
+  terminal.clear()
+  terminal.bold.underline('Downloading:\n')
+  activeDownloadList.slice(-10).forEach((item) => {
+    terminal.cyan(` - ${item.filename}\n`)
+  })
+  // Add padding if list is not full
+  const activePadding = 10 - activeDownloadList.slice(-10).length
+  for (let i = 0; i < activePadding; i++) {
+    terminal('\n')
+  }
+
+  terminal.bold.underline(`\nDownloaded (${totalDownloaded}):\n`)
+  completedDownloadList.slice(-10).forEach((item) => {
+    if (item.status === 'Downloaded') {
+      terminal.green(` - ${item.filename}\n`)
+    } else if (item.status === 'Error' || item.status === 'File Not Found') {
+      terminal.red(` - ${item.filename}: ${item.status}\n`)
+    } else {
+      terminal(` - ${item.filename}: ${item.status}\n`)
+    }
+  })
+  // Add padding if list is not full
+  const completedPadding = 10 - completedDownloadList.slice(-10).length
+  for (let i = 0; i < completedPadding; i++) {
+    terminal('\n')
+  }
+
+  terminal.bold.underline('\nLogs:\n')
+  logs.slice(-20).forEach((log) => {
+    const { message, type } = log;
+    if (type === 'error') {
+      terminal.red(` ${message}\n`)
+    } else if (type === 'info') {
+      terminal.yellow(` ${message}\n`)
+    } else if (type === 'downloading') {
+      terminal.yellow(` ${message}\n`)
+    } else if (type === 'success') {
+      terminal.green(` ${message}\n`)
+    } else {
+      terminal(` ${message}\n`)
+    }
+  })
+  // Add padding if list is not full
+  const logsPadding = 20 - logs.slice(-20).length
+  for (let i = 0; i < logsPadding; i++) {
+    terminal('\n')
+  }
+
+  terminal('\nPress Ctrl+C to exit.\n')
+}
+
+const updateUI = () => {
+  renderUI()
+}
+
+const addToActiveDownloads = (filename) => {
+  activeDownloadList.push({ filename, status: 'Downloading' })
+  if (activeDownloadList.length > 10) {
+    activeDownloadList.shift()
+  }
+  // addLog(`Started downloading ${filename}`, 'downloading')
+  updateUI()
+}
+
+const addToCompletedDownloads = (filename, status) => {
+  completedDownloadList.push({ filename, status })
+  if (completedDownloadList.length > 10) {
+    completedDownloadList.shift()
+  }
+  totalDownloaded += 1
+  if (status === 'Downloaded') {
+    // addLog(`Successfully downloaded ${filename}`, 'success')
+  } else {
+    addLog(`Failed to download ${filename}: ${status}`, 'error')
+  }
+  updateUI()
+}
+
+const addLog = (message, type = 'default') => {
+  logs.push({ message, type })
+  if (logs.length > 20) {
+    logs.shift()
+  }
+  updateUI()
+}
+
+const updateDownloadStatus = (filename, status) => {
+  const download = activeDownloadList.find(item => item.filename === filename)
+  if (download) {
+    download.status = status
+    addToCompletedDownloads(filename, status)
+    activeDownloadList = activeDownloadList.filter(item => item.filename !== filename)
+    updateUI()
+  }
+}
+
+const removeFromActiveDownloads = (filename) => {
+  activeDownloadList = activeDownloadList.filter(item => item.filename !== filename)
+  updateUI()
+}
 
 const processQueue = async () => {
-  while (downloadQueue.length > 0) {
-    const itemsToDownload = downloadQueue.splice(0, 20);
-    for (const item of itemsToDownload) {
-      const cookieString = item.cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; ');
-      const aria2cCommand = `aria2c --dir="${downloadPath}" --header "Cookie: ${cookieString}" --header "Referer: ${item.referer}" "${item.url}" --disable-ipv6=true -x 16 -k 1M'`;
-      // console.log(`COMMAND: ${aria2cCommand}`);
+  while (downloadQueue.length > 0 && activeDownloads < 10) {
+    const item = downloadQueue.shift();
+    activeDownloads++;
+    // Use --dry-run to get the filename
+    const dryRunCommand = `aria2c --dry-run --dir="${downloadPath}" --header "Cookie: ${item.cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; ')}" --header "Referer: ${item.referer}" "${item.url}" --disable-ipv6=true`;
+    exec(dryRunCommand, (error, stdout, stderr) => {
+      if (error) {
+        addLog(`Error executing aria2c dry-run: ${error}`, 'error')
+        activeDownloads--;
+        processQueue();
+        return;
+      }
+
+      const filenameMatch = stdout.match(/Download complete: (.*)/)
+      const filename = filenameMatch ? filenameMatch[1].trim() : 'Unknown'
+
+      addToActiveDownloads(filename.split('/').pop())
+
+      const aria2cCommand = `aria2c --dir="${downloadPath}" --header "Cookie: ${item.cookies.map(cookie => `${cookie.name}=${cookie.value}`).join('; ')}" --header "Referer: ${item.referer}" "${item.url}" --file-allocation=none --disable-ipv6=true`;
       exec(aria2cCommand, async (error, stdout, stderr) => {
+        activeDownloads--;
         if (error) {
-          console.error(`Error executing aria2c: ${error}`);
+          addLog(`Error executing aria2c: ${error}`, 'error')
+          updateDownloadStatus(filename, 'Error')
+          processQueue();
           return;
         }
 
         FILE_PATH = stdout.match(/Download complete: (.*)/)?.[1]?.trim();
 
         if (stderr) {
-          console.error(`aria2c stderr: ${stderr}`);
+          addLog(`aria2c stderr: ${stderr}`, 'info')
         }
 
         // Move the downloaded file to the respective date folder
         const filePath = FILE_PATH
         if (!fs.existsSync(filePath)) {
-          console.log(`File does not exist: ${filePath}`);
+          addLog(`File does not exist: ${filePath}`, 'error')
+          updateDownloadStatus(filename, 'File Not Found')
           return;
         }
         if (!fs.existsSync(filePath)) {
-          console.log(`File does not exist: ${filePath}`);
+          addLog(`File does not exist: ${filePath}`, 'error')
+          updateDownloadStatus(filename, 'File Not Found')
           return;
         }
         let metadata;
         if (fs.existsSync(filePath)) {
-          metadata = await exiftool.read(filePath);
+          try { metadata = await exiftool.read(filePath); } catch (error) { addLog(`Error reading metadata: ${error}`, 'error'); }
         }
         const date = await getMonthAndYear(filePath, metadata, item.page);
         const year = date.year;
@@ -116,8 +253,15 @@ const processQueue = async () => {
         try {
           let newPath = `${downloadPath}/${year}/${month}/${filePath.split('/').pop()}`;
           newPath = validatePath(newPath);
-          await moveFile(filePath, newPath, { overwrite: true });
-          console.log('Download Complete:', `${year}/${month}/${filePath.split('/').pop()}`);
+
+          try {
+            await moveFile(filePath, newPath, { overwrite: true });
+          } catch (error) {
+            addLog(`Error moving file: ${error.message}`, 'error')
+          }
+
+          // addLog(`Download Complete: ${year}/${month}/${filePath.split('/').pop()}`)
+          updateDownloadStatus(filename.split('/').pop(), 'Downloaded')
         } catch (error) {
           const randomNumber = Math.floor(Math.random() * 1000000);
           let newPath = filePath.replace(/(\.[\w\d_-]+)$/i, `_${randomNumber}$1`);
@@ -127,23 +271,30 @@ const processQueue = async () => {
             newPath = truncatePath(newPath)
           }
 
-          await moveFile(filePath, newPath);
-          console.log('Download Complete:', newPath);
+          try {
+            await moveFile(filePath, newPath);
+          } catch (error) {
+            addLog(`Error moving file: ${error.message}`, 'error')
+          }
+          // addLog(`Download Complete: ${newPath}`)
+          updateDownloadStatus(filename, 'Downloaded')
         }
+        processQueue();
       });
-    }
-    await sleep(1000); // Add a delay to avoid overwhelming the system
+    });
   }
 }
 
 const addToQueue = async (url, cookies, referer, page) => {
-  downloadQueue.push({ url, cookies, referer, page });
-  await processQueue();
+  if (downloadQueue.length < 15) {
+    downloadQueue.push({ url, cookies, referer, page });
+    await processQueue();
+  }
 }
 
-(async () => {
+;(async () => {
   const startLink = await getProgress()
-  console.log('Starting from:', new URL(startLink).href)
+  addLog(`Starting from: ${new URL(startLink).href}`, 'info')
 
   const browser = await chromium.launchPersistentContext(path.resolve(userDataDir), {
     headless,
@@ -168,8 +319,8 @@ const addToQueue = async (url, cookies, referer, page) => {
   await page.goto('https://photos.google.com')
 
   const latestPhoto = await getLatestPhoto(page)
-  console.log('Latest Photo:', latestPhoto)
-  console.log('-------------------------------------')
+  addLog(`Latest Photo: ${latestPhoto}`, 'info')
+  addLog('-------------------------------------', 'info')
 
   await page.goto(clean(startLink))
 
@@ -183,44 +334,45 @@ const addToQueue = async (url, cookies, referer, page) => {
     const currentUrl = await page.url()
 
     if (clean(currentUrl) === clean(latestPhoto)) {
-      console.log('-------------------------------------')
-      console.log('Reached the latest photo, exiting...')
+      addLog('-------------------------------------', 'info')
+      addLog('Reached the latest photo, exiting...', 'info')
       break
     }
 
-    /*
-      We click on the left side of arrow in the html. This will take us to the previous photo.
-      Note: I have tried both left arrow press and clicking directly the left side of arrow using playwright click method.
-      However, both of them are not working. So, I have injected the click method in the html.
-    */
-    await page.evaluate(() => document.getElementsByClassName('SxgK2b OQEhnd')[0].click())
+    if (downloadQueue.length < 15 && activeDownloads < 10) {
+      /*
+        We click on the left side of arrow in the html. This will take us to the previous photo.
+        Note: I have tried both left arrow press and clicking directly the left side of arrow using playwright click method.
+        However, both of them are not working. So, I have injected the click method in the html.
+      */
+      await page.evaluate(() => document.getElementsByClassName('SxgK2b OQEhnd')[0].click())
 
-    // we wait until new photo is loaded
+      // we wait until new photo is loaded
 
-    let retries = 3;
-    const retryInterval = 1000;
-    while (retries > 0) {
-      try {
-        await page.waitForURL((url) => {
-          return url.host === 'photos.google.com' && url.href !== currentUrl;
-        }, {
-          timeout: timeoutValue,
-        });
-        break; // Exit loop if successful
-      } catch (error) {
-        retries--;
-        console.error('Error waiting for URL:', error);
-        if (retries === 0) {
-          throw error;
+      let retries = 3;
+      const retryInterval = 1000;
+      while (retries > 0) {
+        try {
+          await page.waitForURL((url) => {
+            return url.host === 'photos.google.com' && url.href !== currentUrl;
+          }, {
+            timeout: timeoutValue,
+          });
+          break // Exit loop if successful
+        } catch (error) {
+          retries--;
+          addLog(`Error waiting for URL: ${error}`, 'error')
+          if (retries === 0) {
+            throw error;
+          }
+          await sleep(retryInterval);
+          continue;
         }
-        await sleep(retryInterval);
-        continue;
       }
+
+      await downloadPhoto(page)
+      await saveProgress(page)
     }
-
-
-    await downloadPhoto(page)
-    await saveProgress(page)
   }
   await browser.close()
   await exiftool.end()
@@ -236,12 +388,18 @@ const downloadPhoto = async (page, overwrite = false) => {
 
   downloadPromise.then(async download => {
     const downloadUrl = download.url();
-    const cookies = await page.context().cookies([downloadUrl]);
     download.cancel();
+    let cookies;
+    if (downloadUrl.includes('photos.fife.usercontent.google.com')) {
+      cookies = await page.context().cookies([downloadUrl]);
+    }
+    else if (downloadUrl.includes('video-downloads.googleusercontent.com')) {
+      cookies = await page.context().cookies();
+    }
     const referer = page.url();
     await addToQueue(downloadUrl, cookies, referer, page);
   }).catch(error => {
-    console.log('Error while waiting for download:', error);
+    addLog(`Error while waiting for download: ${error}`, 'error')
   });
 }
 
